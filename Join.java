ad0bdc7438adbec57c223205e5fd4aed7a838d22
vinodkumarchokkula <vchokkula@unomaha.edu>
2017-03-28 20:56:39 -0500

    Add files via upload

--- Join.java
+++ Join.java
@@ -1,62 +1,130 @@
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Join {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Enter your location\n");
 		
-		String name = System.console().readLine();
+		/*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter your location\n");
+        String inp = br.readLine();
+		String name = System.console().readLine();*/
 		locationData l=new locationData();
 		l.splitData();
-		if(name=="NYC"){
-			System.out.println("Enter your location\n");
-			System.out.println("Select query\n");
-			
+		
+			/*System.out.println("Enter your location\n");
+			System.out.println("Select query\n");*/
+		/*	
 			String queryno = System.console().readLine();
-			int qry=Integer.parseInt(queryno);
-			if(qry==1){	
+			int qry=Integer.parseInt(queryno);*/
+			/*if(qry==1){	*/
 			int outputBlock=1;
 			
-		HashMap<String,String> Result=new HashMap<String,String>();
+		HashMap<String,String> Result1=new HashMap<String,String>();
 			for (String o:locationData.NYCdepositorBlocks.keySet()){
 				for(String i:locationData.HOUcustomerBlocks.keySet()){
 					locationData.NYCdepositorBlocks.get(o);
-					for(String s:locationData.NYCdepositorBlocks.get(o).keySet())
+					for(String s:locationData.NYCdepositorBlocks.get(o).values())
 							{
 						for(String p:locationData.HOUcustomerBlocks.get(i).keySet()){
-							if(s.equals(p)){
+							
+							if(s .equals(p)){
 								//String temp=locationData.HOUcustomerBlock1.keySet();
-								if(Result.size()<5){
-									Result.put(s,locationData.HOUcustomerBlocks.get(i).get(s));
+								if(Result1.size()<5){
+									Result1.put(s,locationData.HOUcustomerBlocks.get(i).get(s));
+									
 								}
-								else if(Result.size()==5){
+								else if(Result1.size()==5){
 									outputBlock++;
 									System.out.println("Output block is full...result is written in file"+outputBlock);
 									
-											FileWriter fw = new FileWriter("C:/Users/Vinod Chokkula/dbproj/JoinImplementation/output file");
-											BufferedWriter bw = new BufferedWriter(fw);
-									bw.write(Result.toString());
-									Result.clear();
-									Result.put(s,locationData.HOUcustomerBlocks.get(i).get(s));
+									PrintWriter writer = new PrintWriter("C:/Users/Vinod Chokkula/dbproj/JoinImplementation/output file", "UTF-8");
+								    writer.println(Result1.toString());
+								    writer.close();
+									/*FileWriter fw = new FileWriter("C:/Users/Vinod Chokkula/dbproj/JoinImplementation/output file");
+									BufferedWriter bw = new BufferedWriter(fw);
+									bw.write(Result1.toString());*/
+									Result1.clear();
+									Result1.put(s,locationData.HOUcustomerBlocks.get(i).get(s));
 								}
 								
 							}
 						}
-						System.out.println(Result.size());
+						
 							}
 				}
-				System.out.println(Result);
+				///System.out.println(Result1);
+			}
+			
+			HashMap<String,String> Result2=new HashMap<String,String>();
+			for (String o:locationData.NYCdepositorBlocks.keySet()){
+				for(String i:locationData.NYCaccountBlocks.keySet()){
+					for(String s:locationData.NYCdepositorBlocks.get(o).keySet())
+					{
+						for(String p:locationData.NYCaccountBlocks.get(i).keySet()){
+					/*System.out.println("S#P"+s+p);*/
+					if(s .equals(p)){
+						String temp=locationData.NYCaccountBlocks.get(i).get(p);
+						String cname=locationData.NYCdepositorBlocks.get(o).get(s);
+						/*System.out.println(temp);*/
+						String T[]=temp.split(":");
+						Result2.put(cname,T[1]);
+						System.out.println("result2"+Result2);
+						/*if(Result2.size()<5){
+							Result2.put(s,p);
+						}
+						else if(Result1.size()==5){
+							re
+						}*/
+						}
+					}
+					
+				}
+				}
+				}
+				
+			HashMap<String,String> Result3=new HashMap<String,String>();
+			ArrayList<String> Join3list= new ArrayList<String>();
+			for(String o:locationData.SFOAccountBlocks.keySet()){
+				for(String i:locationData.NYCdepositorBlocks.keySet()){
+					for(String s:locationData.SFOAccountBlocks.get(o).keySet())
+					{
+						for(String p:locationData.NYCdepositorBlocks.get(i).keySet()){
+					
+					if(s.equalsIgnoreCase(p)){
+						Join3list.add(locationData.NYCdepositorBlocks.get(i).get(p));
+					}
+						}
+					}
+						}
 				
 			}
+			for (String q:Join3list){
+				for(String i:locationData.HOUcustomerBlocks.keySet()){
+					
+						for(String p:locationData.HOUcustomerBlocks.get(i).keySet()){
+								if(q.equalsIgnoreCase(p)){
+								Result3.put(p,locationData.HOUcustomerBlocks.get(i).get(p));	
+								}
+								
+								}
+						System.out.println("Hou"+Result3);
 			}
+			}
+			
+			
+			
+			
+			
+				}
+				
+			}
+				
+			
+			
 
-			if(qry==2){
-				
-			}
-	}
-	}
-}
+
--- locationData.java
+++ locationData.java
@@ -4,12 +4,17 @@ import java.util.*;
 import java.util.Scanner;
 
 public class locationData {
+	
+	
 	public static HashMap<String,String> NYCdepositorBlock1 =new HashMap<String,String>();
 	public static HashMap<String,String> NYCdepositorBlock2 =new HashMap<String,String>();
 	public static HashMap<String,String> NYCdepositorBlock3 =new HashMap<String,String>();
 	public static HashMap<String,String> NYCdepositorBlock4 =new HashMap<String,String>();
+	public static HashMap<String,String> OMAdepositorBlock1 =new HashMap<String,String>();
+	
 	public static HashMap<String,String> NYClocationMap =new HashMap<String,String>();
 	public static HashMap<String,String> NYCaccountMap =new HashMap<String,String>();
+	
 	//public static HashMap<String,String> HOUcustomerMap =new HashMap<String,String>();
 	public static HashMap<String,String> HOUcustomerBlock1 =new HashMap<String,String>();
 	public static HashMap<String,String> HOUcustomerBlock2 =new HashMap<String,String>();
@@ -21,6 +26,7 @@ public class locationData {
 	public static HashMap<String,String> HOUcustomerBlock8 =new HashMap<String,String>();
 	
 	public static HashMap<String,String> Nycbranchblock = new HashMap<String,String>();
+	public static HashMap<String,String> SFObranchblock = new HashMap<String,String>();
 	
 	public static HashMap<String,String> NYCaccountblock1 =new HashMap<String,String>();
 	public static HashMap<String,String> NYCaccountblock2 =new HashMap<String,String>();
@@ -28,14 +34,24 @@ public class locationData {
 	public static HashMap<String,String> NYCaccountblock4 =new HashMap<String,String>();
 	public static HashMap<String,String> NYCaccountblock5 =new HashMap<String,String>();
 	public static HashMap<String,String> NYCaccountblock6 =new HashMap<String,String>();
+	public static HashMap<String,String> SFOAccountblock1 =new HashMap<String,String>();
+	public static HashMap<String,String> OMAAccountblock1 =new HashMap<String,String>();
+	public static HashMap<String,String> OMAAccountblock2 =new HashMap<String,String>();
+	
+	
+
+	public static HashMap<String,HashMap<String,String>> SFOAccountBlocks =new HashMap<String,HashMap<String,String>>();
 	
 	public static HashMap<String,HashMap<String,String>> NYCdepositorBlocks =new HashMap<String,HashMap<String,String>>();
 	public static HashMap<String,HashMap<String,String>> HOUcustomerBlocks =new HashMap<String,HashMap<String,String>>();
 	public static HashMap<String,HashMap<String,String>> NYCaccountBlocks =new HashMap<String,HashMap<String,String>>();
+	public static HashMap<String,HashMap<String,String>> OMAaccountBlocks =new HashMap<String,HashMap<String,String>>();
+	public static HashMap<String,HashMap<String,String>> OMADepositorBlocks =new HashMap<String,HashMap<String,String>>();
+	
+	
 	public void splitData() throws FileNotFoundException{
 	File depositorfile = new File("C:/Users/Vinod Chokkula/dbproj/JoinImplementation/src/Depositor.csv");
 	File locationfile = new File("C:/Users/Vinod Chokkula/dbproj/JoinImplementation/src/Depositor.csv");
-	//
 	File customerfile = new File("C:/Users/Vinod Chokkula/dbproj/JoinImplementation/src/Customer.csv");
 	
 	File branchfile= new File("C:/Users/Vinod Chokkula/dbproj/JoinImplementation/src/branch.csv");
@@ -46,63 +62,89 @@ public class locationData {
 	Scanner sc=new Scanner(depositorfile);
 	while(sc.hasNextLine()){
 	String[] depositorArray=sc.nextLine().split(",");
+	
 	if(depcount==0 || depcount<15){
-	NYCdepositorBlock1.put(depositorArray[0], depositorArray[1]);
-	depcount++;
+		NYCdepositorBlock1.put(depositorArray[1], depositorArray[0]);
+		
+		depcount++;
+		
 	}
 	else if(depcount>=15 && depcount<30){
-		NYCdepositorBlock2.put(depositorArray[0], depositorArray[1]);
+		NYCdepositorBlock2.put(depositorArray[1], depositorArray[0]);
 		depcount++;
+		
 	}
 	else if(depcount>=30 && depcount<45){
-		NYCdepositorBlock3.put(depositorArray[0], depositorArray[1]);
+		NYCdepositorBlock3.put(depositorArray[1], depositorArray[0]);
 		depcount++;
+		
+	}
+	else{
+		NYCdepositorBlock4.put(depositorArray[1], depositorArray[0]);
+		depcount++;
+		
+	}
+	if( depositorArray[1].equalsIgnoreCase("A10300") || 
+			depositorArray[1].equalsIgnoreCase("A10301") ||
+			depositorArray[1].equalsIgnoreCase("A10302") ||
+			depositorArray[1].equalsIgnoreCase("A10303") ||
+			depositorArray[1].equalsIgnoreCase("A10304") ||
+			depositorArray[1].equalsIgnoreCase("A10305") ||
+			depositorArray[1].equalsIgnoreCase("A10306") ||
+			depositorArray[1].equalsIgnoreCase("A10307") ||
+			depositorArray[1].equalsIgnoreCase("A10308") ||
+			depositorArray[1].equalsIgnoreCase("A10309") ||
+			depositorArray[1].equalsIgnoreCase("A10339") ||
+			depositorArray[1].equalsIgnoreCase("A10340") ||
+			depositorArray[1].equalsIgnoreCase("A10341") ){
+			
+		OMAdepositorBlock1.put(depositorArray[1], depositorArray[0]);
+		 
 	}
 	
-	else{
-		NYCdepositorBlock4.put(depositorArray[0], depositorArray[1]);
-		depcount++;
-	}
+	
 	}
 	sc.close();
 	NYCdepositorBlocks.put("1", NYCdepositorBlock1);
 	NYCdepositorBlocks.put("2", NYCdepositorBlock2);
 	NYCdepositorBlocks.put("3", NYCdepositorBlock3);
 	NYCdepositorBlocks.put("4", NYCdepositorBlock4);
+	OMADepositorBlocks.put("1",OMAdepositorBlock1);
+	
 	sc=new Scanner(customerfile);
 	while(sc.hasNextLine()){
 	String[] customerArray=sc.nextLine().split(",");
 	String nonKeyValue=customerArray[1]+":"+customerArray[2];
 	
-	if(depcount==0 || depcount<7){
+	if(custcount==0 || custcount<=7){
 		HOUcustomerBlock1.put(customerArray[0], nonKeyValue);
 		custcount++;
 		}
-		else if(depcount>=8 && depcount<15){
+		else if(custcount>=8 && custcount<=15){
 			HOUcustomerBlock2.put(customerArray[0], nonKeyValue);
 			custcount++;
 		}
-		else if(depcount>=16 && depcount<23){
+		else if(custcount>=16 && custcount<=23){
 			HOUcustomerBlock3.put(customerArray[0], nonKeyValue);
 			custcount++;
 		}
-		else if(depcount>=24 && depcount<31){
+		else if(custcount>=24 && custcount<=31){
 			HOUcustomerBlock4.put(customerArray[0], nonKeyValue);
 			custcount++;
 		}
-		else if(depcount>=32 && depcount<39){
+		else if(custcount>=32 && custcount<=39){
 			HOUcustomerBlock5.put(customerArray[0], nonKeyValue);
 			custcount++;
 		}
-		else if(depcount>=40 && depcount<47){
+		else if(custcount>=40 && custcount<=47){
 			HOUcustomerBlock6.put(customerArray[0], nonKeyValue);
 			custcount++;
 		}
-		else if(depcount>=48 && depcount<55){
+		else if(custcount>=48 && custcount<=55){
 			HOUcustomerBlock7.put(customerArray[0], nonKeyValue);
 			custcount++;
 		}
-		else if(depcount>=56 && depcount<64){
+		else if(custcount>=56 && custcount<=64){
 			HOUcustomerBlock8.put(customerArray[0], nonKeyValue);
 			custcount++;
 		}
@@ -121,9 +163,11 @@ public class locationData {
 	
 	//branch Block
 	 sc=new Scanner(branchfile);
+	 while(sc.hasNextLine()){
 	 String[] branchArray=sc.nextLine().split(",");
 	 String temp=branchArray[1]+','+branchArray[2];
 	 Nycbranchblock.put(branchArray[0],temp);
+	 }
 	 sc.close();
 
 	 //Account Blocks
@@ -132,45 +176,88 @@ public class locationData {
 		while(sc.hasNextLine()){
 		String[] accountArray=sc.nextLine().split(",");
 		String nonKeyValue=accountArray[1]+":"+accountArray[2];
+		if(accountArray[1].equalsIgnoreCase("Chinatown")){
+			SFOAccountblock1.put(accountArray[0], nonKeyValue);	
+		}
+		if(accountArray[1].equalsIgnoreCase("Aksarben") || accountArray[1].equals("102 Street")){
+			if(OMAAccountblock1.size()!=10){
+				OMAAccountblock1.put(accountArray[0], nonKeyValue);
+			}
+			else if((OMAAccountblock1.size()!=10)){OMAAccountblock2.put(accountArray[0], nonKeyValue);}
+		}
 		
-		if(depcount==0 || depcount<10){
+		if(acccount==0 || acccount<10){
 			NYCaccountblock1.put(accountArray[0], nonKeyValue);
 			acccount++;
 			}
-			else if(depcount>=10 && depcount<20){
+			else if(acccount>=10 && acccount<20){
 				NYCaccountblock2.put(accountArray[0], nonKeyValue);
 				acccount++;
+				
 			}
-			else if(depcount>=20 && depcount<30){
+			else if(acccount>=20 && acccount<30){
 				NYCaccountblock3.put(accountArray[0], nonKeyValue);
 				acccount++;
+				
 			}
-			else if(depcount>=30 && depcount<40){
+			else if(acccount>=30 && acccount<40){
 				NYCaccountblock4.put(accountArray[0], nonKeyValue);
 				acccount++;
+				
 			}
-			else if(depcount>=40 && depcount<50){
+			else if(acccount>=40 && acccount<50){
 				NYCaccountblock5.put(accountArray[0], nonKeyValue);
 				acccount++;
+				
 			}
-			else if(depcount>=50 && depcount<60){
+			else if(acccount>=50 && acccount<60){
 				NYCaccountblock6.put(accountArray[0], nonKeyValue);
 				acccount++;
+				
 			}
 			
 			
 		NYCaccountBlocks.put("1", NYCaccountblock1);
-		NYCaccountBlocks.put("2", NYCaccountblock1);
-		NYCaccountBlocks.put("3", NYCaccountblock1);
-		NYCaccountBlocks.put("4", NYCaccountblock1);
-		NYCaccountBlocks.put("5", NYCaccountblock1);
-		NYCaccountBlocks.put("6", NYCaccountblock1);
+		NYCaccountBlocks.put("2", NYCaccountblock2);
+		NYCaccountBlocks.put("3", NYCaccountblock3);
+		NYCaccountBlocks.put("4", NYCaccountblock4);
+		NYCaccountBlocks.put("5", NYCaccountblock5);
+		NYCaccountBlocks.put("6", NYCaccountblock6);
 		
+		SFOAccountBlocks.put("1", SFOAccountblock1);
 		
+		OMAaccountBlocks.put("1", OMAAccountblock1);
+		OMAaccountBlocks.put("2", OMAAccountblock2);
 		}
 		sc.close();
 		
+		SFObranchblock=Nycbranchblock;
+		
+		
+		System.out.println("SFO Branch :"+SFObranchblock);
+		System.out.println("==========================");
+		System.out.println("OMA account :"+OMAaccountBlocks);
+		System.out.println("==========================");
+		System.out.println("SFO ACCOunt :"+SFOAccountBlocks);
+		System.out.println("==========================");
+		System.out.println("NYC depositor :"+NYCdepositorBlocks);
+		System.out.println("==========================");
+		System.out.println("HOU customer :"+HOUcustomerBlocks);
+		System.out.println("==========================");
+		System.out.println("NYC ACCOunt :"+NYCaccountBlocks);
+		System.out.println("==========================");
+		System.out.println("OMA dep :"+OMADepositorBlocks);
+		System.out.println("==========================");
+		System.out.println("NYC branch :"+Nycbranchblock);
+		System.out.println("==========================");
+		
+		
+		
+		
+		
 	}
+	
+
 	
 	
 }
